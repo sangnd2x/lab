8744b34c621a5c1d4b1bbd0a7c7a2f638a388b82
sangnd2x <sanguyen128@gmail.com>
2022-07-12 14:02:27 +0700

    Redux thunk and logger commit logger not working and pages just keep loading forever and won't show dish

--- src/redux/ActionCreator.js
+++ src/redux/ActionCreator.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
+import {DISHES} from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
